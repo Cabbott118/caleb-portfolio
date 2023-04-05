0315d9b75697010e3d42fc694d4796e05c79d1ee
unknown <calebhaabbott94@gmail.com>
2023-04-05 14:27:09 -0400

    Added basic mui theme

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
 import './App.css';
 
+// MUI
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+
+// Utility
+import themeFile from './utility/theme';
+
+const theme = createTheme(themeFile);
+
 function App() {
-  return <div className='App'>Hello world</div>;
+  return (
+    <ThemeProvider theme={theme}>
+      <div className='App'></div>
+    </ThemeProvider>
+  );
 }
 
 export default App;
---
+++ src/utility/theme.js
@@ -0,0 +1,24 @@
+export default {
+  palette: {
+    primary: {
+      main: '#9cc9ad',
+    },
+    secondary: {
+      main: '#fff',
+    },
+  },
+  breakpoints: {
+    values: {
+      xs: 0,
+      sm: 600,
+      md: 960,
+      lg: 1280,
+      xl: 1920,
+    },
+  },
+  spreadThis: {
+    typography: {
+      useNextVariants: true,
+    },
+  },
+};
